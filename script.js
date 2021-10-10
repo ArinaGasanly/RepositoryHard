df1d2158a5d124fc1e85af4eece369d54764b4b2
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-10 18:16:49 +0300

    добавление 4 усложнённого урока

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-// 1 задание
+function getWord() {
 
-const lang = prompt('Какой язык?', 'en, ru');
-const ruDays = ('понедельник, вторник среда, четверг, пятница, суббота, воскресенье');
-const enDays = ('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+  let str = ' JavaScript is a text-based programming language used both on the client-side and server-side that allows you to make web pages interactive. ';
+  if (typeof str === 'string' || str instanceof String) {
+    console.log('Variable is a string');
+  } else {
+    console.log('Variable is not a string');
+    
+  }
 
-
-if (lang == 'ru') {
-  console.log(ruDays);
-
-} else (lang == 'en'); {
-  console.log(enDays);
+  return str.trim() + str.slice(0, 30) + '...';
 
 }
 
-
-switch(lang) {
-  case'ru':
-  console.log(ruDays);
-  break;
-  case 'en':
-  console.log(enDays);
-  break;
-  default:
-  console.log('Нет никаких значений');
-}
-
-
-
-let map = new Map([
-  ['ru', 'понедельник, вторник среда, четверг, пятница, суббота, воскресенье'],
-  ['en', 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']
-]);
-
-
-console.log(map.get('ru') );
-console.log(map.get('en') );
-
-
-// 2 задание
-
-const namePerson = prompt('Как зовут?', '');
-const message = (namePerson == 'Александр') ? 'преподаватель' :
-  (namePerson == 'Артём') ? 'директор' : 'Всё ещё ожидаю твой ответ!';
-console.log(message);
+getWord();
