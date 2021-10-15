2de687516e052c0373e53a4e6207712e559838ce
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-15 21:35:15 +0300

    добавление 7 усложнённого урока

---
+++ script.js
@@ -0,0 +1,15 @@
+const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const todayDay = new Date();
+
+
+for (let i = 0, len = week.length; i < len; i++) {
+
+  let html = week[i]; 
+  if (i === +todayDay.getDay()) html = html.italics();
+  else if (i > 4) html = html.bold(); 
+
+  const div = document.createElement('div');
+  div.innerHTML = html;
+  document.body.appendChild(div);
+
+  }
