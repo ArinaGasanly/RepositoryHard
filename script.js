2dc76343165ba122a3470fe5b3ea3884f0a20614
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-10-15 21:42:04 +0300

    добавление 7 усложнённого урока

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const todayDay = new Date();
 for (let i = 0, len = week.length; i < len; i++) {
 
   let html = week[i]; 
-  if (i === +todayDay.getDay()) html = html.italics();
+  if (i === +todayDay.getDay()-1) html = html.italics();
   else if (i > 4) html = html.bold(); 
 
   const div = document.createElement('div');
